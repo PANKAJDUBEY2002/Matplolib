d91e31ab0c6bb6b5e83f97902d9fc2b64d421a3b
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2023-12-29 19:22:48 +0530

    draw a plot graph

---
+++ plot.py
@@ -0,0 +1,18 @@
+# import pyplot
+from matplotlib import pyplot as plt
+
+
+#set graph prop
+x=[1,2,3,4,5]
+y=[23,13,56,45,100]
+plt.plot(x,y)
+plt.xlabel("Test")
+plt.ylabel("Marks")
+plt.title("Student Marks")
+
+
+
+
+
+#show graph
+plt.show()
