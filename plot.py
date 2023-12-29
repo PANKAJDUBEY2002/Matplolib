92f338654dac24c54f6e983e4b889fa48af45638
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2023-12-30 00:44:00 +0530

    plot on multiple data

--- plot.py
+++ plot.py
@@ -4,12 +4,14 @@ from matplotlib import pyplot as plt
 
 #set graph prop
 x=[1,2,3,4,5]
-y=[23,13,56,45,100]
-plt.plot(x,y)
+y1=[23,13,56,45,100]
+y2=[13,76,23,97,54]
+plt.plot(x,y1,label='Data')
+plt.plot(x,y2,label='Pankaj')
 plt.xlabel("Test")
 plt.ylabel("Marks")
 plt.title("Student Marks")
-
+plt.legend()
 
 
 
